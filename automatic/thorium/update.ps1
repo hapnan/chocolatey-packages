64a508afc44d551437ad9455a026bc0e03033d31
HapnanAR <hapnanarsad@gmail.com>
2024-01-22 21:27:25 +0700

    AvoidUsingCmdletAliases

--- automatic/thorium/update.ps1
+++ automatic/thorium/update.ps1
@@ -14,7 +14,7 @@ function global:au_GetLatest {
   $download_page = Invoke-RestMethod -Method GET -Uri $releases
   $filenamePattern = "*.exe"
   $version = $download_page.tag_name -replace '[a-zA-Z]', '' 
-  $url = ($download_page.assets | Where-Object name -like $filenamePattern ).browser_download_url | select -First 1
+  $url = ($download_page.assets | Where-Object name -like $filenamePattern ).browser_download_url | Select-Object -First 1
   
 
   @{
