bfa9dbd159dc61ae4aadd736de3e7f17e7a99853
HapnanAR <hapnanarsad@gmail.com>
2024-01-22 14:21:43 +0700

    remove unused variable

--- automatic/thorium/tools/chocolateyinstall.ps1
+++ automatic/thorium/tools/chocolateyinstall.ps1
@@ -1,5 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'thorium';
 $fileType    = 'exe';
@@ -7,26 +8,17 @@ $url        = 'https://github.com/Alex313031/Thorium-Win/releases/latest/downloa
 $checksum   = '6324808A60094A48FCEDB382ECB1125759277E1B9AF1FBBCDED0732DCE7731FD';
 $checksumTypeAll= 'sha256';
 $softwareName   = 'thorium*';
+$silentArgs = '--do-not-launch-chrome';
 
-$registryPathThroiumAsUser = 'hkcu:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Thorium';
-$isThroiumInstallAsUser    = Test-Path $registryPathThroiumAsUser;
- 
-if ($isThroiumInstallAsUser) {
-  $silentArgs = '--do-not-launch-chrome';
-} else {
-  $silentArgs = '--do-not-launch-chrome';
-}
+
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = $fileType #only one of these: exe, msi, msu
-  url           = $url
-  url64bit      = $url
-  softwareName  = $softwareName  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType      = $fileType 
+  url           = $url  
+  softwareName  = $softwareName
   checksum      = $checksum
-  checksumType  = $checksumTypeAll #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = $checksum
-  checksumType64= $checksumTypeAll #default is checksumType
+  checksumType  = $checksumTypeAll
   silentArgs    = $silentArgs 
   validExitCodes= @(0)
 }
